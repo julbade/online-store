1d5c08a9f6e943d35ec8bfbd5c8923d730a1289d
Guest User <Guest@epicodus-25.local>
2018-10-30 11:24:07 -0700

    added add function for database

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { AboutComponent } from './about/about.component';
 import { MarketplaceComponent } from './marketplace/marketplace.component';
 import { AlbumDetailComponent } from './album-detail/album-detail.component';
+import { AdminComponent }   from './admin/admin.component';
+
+
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'about', component: AboutComponent},
   { path: 'marketplace', component: MarketplaceComponent},
-  { path: 'albums/:id', component: AlbumDetailComponent}
+  { path: 'albums/:id', component: AlbumDetailComponent},
+  { path: 'admin', component: AdminComponent}
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,19 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { AboutComponent } from './about/about.component';
 import { MarketplaceComponent } from './marketplace/marketplace.component';
 import { AlbumDetailComponent } from './album-detail/album-detail.component';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AdminComponent } from './admin/admin.component';
+
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
+
 
 @NgModule({
   declarations: [
@@ -15,13 +28,16 @@ import { AlbumDetailComponent } from './album-detail/album-detail.component';
     WelcomeComponent,
     AboutComponent,
     MarketplaceComponent,
-    AlbumDetailComponent
+    AlbumDetailComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+   AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
